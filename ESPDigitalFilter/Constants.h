e73b7d350b80c044dfe2d79a485bd02bf8d8553a
CavRiley <cavan-riley@uiowa.edu>
2024-11-03 20:10:35 -0600

    ADD: added constants file

---
+++ ESPDigitalFilter/Constants.h
@@ -0,0 +1,11 @@
+#define WIFI_SSID ""
+#define WIFI_USERNAME ""
+#define WIFI_PASSWORD ""
+
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 465
+
+#define AUTHOR_EMAIL ""
+#define AUTHOR_PASSWORD ""
+
+#define RECIPIENT_EMAIL ""
